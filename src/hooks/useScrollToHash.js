8c8a623de5a0cf10e840d9b70140bf8bd0571f5b
Roman Ferrero <romaferrero@hotmail.com>
2026-05-07 16:40:02 -0300

    feat(hooks): useContactForm con validacion y useScrollToHash

---
+++ src/hooks/useScrollToHash.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+/**
+ * Si la URL trae un hash (ej: /#nosotros), hace scroll suave al elemento.
+ */
+export default function useScrollToHash() {
+  const { hash, pathname } = useLocation()
+
+  useEffect(() => {
+    if (hash) {
+      const id = hash.replace('#', '')
+      const el = document.getElementById(id)
+      if (el) {
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+        }, 80)
+      }
+    } else {
+      window.scrollTo({ top: 0, behavior: 'instant' })
+    }
+  }, [hash, pathname])
+}
