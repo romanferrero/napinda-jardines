721832f6fb4997452c7a8f94acd08c3ddfdfb64c
Roman Ferrero <romaferrero@hotmail.com>
2026-05-07 16:40:04 -0300

    feat: routing, HomePage que compone secciones y NotFoundPage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import Hero from '../sections/Hero'
+import About from '../sections/About'
+import Team from '../sections/Team'
+import Services from '../sections/Services'
+import Gallery from '../sections/Gallery'
+import Contact from '../sections/Contact'
+import WhatsAppFAB from '../sections/WhatsAppFAB'
+
+/**
+ * Página principal: compone todas las secciones en orden.
+ */
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Team />
+      <Services />
+      <Gallery />
+      <Contact />
+      <WhatsAppFAB />
+    </>
+  )
+}
